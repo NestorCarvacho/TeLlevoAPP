9d8536c2ce21885ac80a4b6f5a3cdd94d32053a6
Kumatetsu <nestor.carvacho@gmail.com>
2023-09-11 19:27:09 -0300

    sdaasdasdasdas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,36 +2,25 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   loadChildren: () =>
-  //     import('./splash/splash.module').then((m) => m.SplashPageModule),
-  // },
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
   {
     path: 'splash',
-    loadChildren: () =>
-      import('./splash/splash.module').then((m) => m.SplashPageModule),
+    loadChildren: () => import('./splash/splash.module').then( m => m.SplashPageModule)
   },
   {
     path: 'login',
-    loadChildren: () =>
-      import('./login/login.module').then((m) => m.LoginPageModule),
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
-  {
-    path: 'tabs',
-    loadChildren: () =>
-      import('./tabs/tabs.module').then((m) => m.TabsPageModule),
-  },
-  {
-    path: '',
-    redirectTo: 'splash',
-    pathMatch: 'full',
-  },
+
+
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
