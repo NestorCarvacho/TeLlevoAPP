f20160dba9c56c4927ac544ab11367beba538457
Nicolás Muñoz <n.munoz7@duocuc.cl>
2023-09-13 00:25:16 -0300

    wena loco ahora si no hay angular material jaja saludos
    sdfdfgfdfdgfdgffdbfgbfvgfgbdcv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatButtonModule } from '@angular/material/button';
+
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,MatIconModule,MatDividerModule,MatButtonModule,],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/tab3/editar-nombre/editar-nombre.page.ts
+++ src/app/tab3/editar-nombre/editar-nombre.page.ts
@@ -11,5 +11,6 @@ export class EditarNombrePage implements OnInit {
 
   ngOnInit() {
   }
+  
 
 }
