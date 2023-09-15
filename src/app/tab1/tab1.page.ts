93e34c23bd7d3cb128b9f5a00d408b2b6d5704c6
Kumatetsu <nestor.carvacho@gmail.com>
2023-09-14 22:55:54 -0300

    nicolas medeina

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,10 +7,20 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
   presentingElement: Element | null = null;
+  presentingElement2: Element | null = null;
 
   constructor() {}
 
   ngAfterViewInit() {
+    // Obtén la referencia al elemento DOM después de que la vista se haya inicializado.
+    this.presentingElement2 = document.querySelector('.ion-page');
+    // Verifica si se encontró el elemento.
+    if (this.presentingElement2) {
+      // Haz algo con this.presentingElement aquí
+    } else {
+      console.error("No se encontró ningún elemento con la clase '.ion-page'");
+    }
+    
     // Obtén la referencia al elemento DOM después de que la vista se haya inicializado.
     this.presentingElement = document.querySelector('.ion-page');
     // Verifica si se encontró el elemento.
--- src/app/tab1/viajar/viajar.page.ts
+++ src/app/tab1/viajar/viajar.page.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./viajar.page.scss'],
 })
 export class ViajarPage implements OnInit {
+  presentingElement: Element | null = null;
 
   constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngAfterViewInit() {
+    // Obtén la referencia al elemento DOM después de que la vista se haya inicializado.
+    this.presentingElement = document.querySelector('.ion-page');
+    // Verifica si se encontró el elemento.
+    if (this.presentingElement) {
+      // Haz algo con this.presentingElement aquí
+    } else {
+      console.error("No se encontró ningún elemento con la clase '.ion-page'");
+    }
   }
 
 }
