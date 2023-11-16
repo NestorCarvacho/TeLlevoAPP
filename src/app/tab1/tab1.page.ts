e87a4e24d744236e49c1eb36af90138a1f830a93
Kumatetsu <nestor.carvacho@gmail.com>
2023-11-15 23:59:15 -0300

    asd

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,16 +7,17 @@ import { environment } from 'src/environments/environment';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
 })
+
 export class Tab1Page implements AfterViewInit {
   lat: number = -33.59813725425291;
   lng: number = -70.5782624941369;
 
-  public popUp = new mapboxgl.Popup().setHTML("<p>Tu ubicación!</p>");
+  public popUp = new mapboxgl.Popup().setHTML('<p>Tu ubicación!</p>');
   public marker = new mapboxgl.Marker({
     draggable: true,
     scale: 1,
   });
-  
+
   searchTerm: string = '';
   presentingElement: Element | null = null;
   presentingElement2: Element | null = null;
@@ -35,6 +36,15 @@ export class Tab1Page implements AfterViewInit {
     if (!this.presentingElement) {
       console.error("No se encontró ningún elemento con la clase '.ion-page'");
     }
+
+    const list: NodeListOf<Element> = document.querySelectorAll(".list");
+
+    function activeLink(this: HTMLElement) {
+      list.forEach((item: Element) => item.classList.remove("active"));
+      this.classList.add("active");
+    }
+
+    list.forEach((item: Element) => item.addEventListener("click", activeLink));
   }
 
   constriurMapa() {
