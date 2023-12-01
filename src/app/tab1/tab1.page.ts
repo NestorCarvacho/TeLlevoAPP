6e27010bd2f94cae1153695c8dfb34db0d825b4e
Nicolás Muñoz <n.munoz7@duocuc.cl>
2023-11-30 21:17:35 -0300

    merge

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,7 @@
-<<<<<<< Updated upstream
-import { Component, AfterViewInit } from '@angular/core';
-=======
 // import { Tab1Page } from './tab1.page';
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { environment } from 'src/environments/environment';
->>>>>>> Stashed changes
 import * as mapboxgl from 'mapbox-gl';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-tab1',
